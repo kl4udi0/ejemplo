9a269f8d0a8afe7f2165a5c5111afd535054a909
kl4udi0 <43246217+kl4udi0@users.noreply.github.com>
2023-04-21 18:43:04 -0400

    Perfecta. Pendiente añadir más free products
    Se requiere mejorar para que añada más de un producto gratis.

--- ejemplo.php
+++ ejemplo.php
@@ -1,9 +1,7 @@
-// Add free gifted product for specific products in the cart Rutina Rejuvenecedora Priori
 add_action( 'woocommerce_before_calculate_totals', 'check_free_gifted_product_promo_rejuvenecedora' );
 function check_free_gifted_product_promo_rejuvenecedora( $cart ) {
-	
-	global $is_target_product_id;
-	
+    global $is_target_product_id;
+
     if ( is_admin() && ! defined( 'DOING_AJAX' ) )
         return;
 
@@ -15,14 +13,12 @@ function check_free_gifted_product_promo_rejuvenecedora( $cart ) {
     $free_product_id   = array (2817, 5870); //Gentle Cleanser y Neceser Priori
     $target_product_id_rutina = 5853; //(Rutina Rejuvenecedora)
     $target_product_id_individuales = array (5319, 2814); //(Moisturizing Cream y Brightening Serum)
-	$is_target_product_in_cart = false;
+    $is_target_product_in_cart = false;
 
     $start_time = strtotime('2023-04-20 12:15:00'); // Establezca la fecha y hora de inicio aquí
     $expiration_time = strtotime('2023-04-30 23:59:59'); // Establezca la fecha y hora de expiración aquí
-	//Preparar $time_ok = current_time('timestamp') > $start_time && current_time('timestamp') < $expiration_time
-	
-    $cart_subtotal     = 0; // Initializing
 
+    // Loop through cart items (first loop)
     foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
         // Getting an instance of the product object
         $product =  $cart_item['data'];
@@ -31,66 +27,46 @@ function check_free_gifted_product_promo_rejuvenecedora( $cart ) {
         if ($cart_item['product_id'] == $target_product_id_rutina || in_array($cart_item['product_id'], $target_product_id_individuales) ){
             $is_target_product_id = true;
             $key_to_remove = $cart_item_key;
-			$is_target_product_in_cart = true;
+            $is_target_product_in_cart = true;
         }
 
         // When free product is in cart
-		elseif ( in_array( $cart_item['product_id'], $free_product_id ) && $is_target_product_id == true ) {
-            $free_key = $cart_item_key;
-            $free_qty = $cart_item['quantity'];
+        elseif ( in_array( $cart_item['product_id'], $free_product_id ) && $is_target_product_id == true ) {
+            if ($cart_item['quantity'] > 1) {
+                // Reset quantity to 1 if it has been modified
+                $cart->set_quantity($cart_item_key, 1);
+            }
             $cart_item['data']->set_price(0); // Optionally set the price to zero
-        } else {
-            //$cart_subtotal += $cart_item['line_total'] + $cart_item['line_tax'];
         }
     }
 
-    // If the free product is not already in cart, subtotal match and the customer insn't added an excluded product, addd it
-    if ( ! isset($free_key) && $product->is_on_sale() == false && ( $cart_item['product_id'] == 5853 || in_array($cart_item['product_id'], $target_product_id_individuales) ) && current_time('timestamp') > $start_time && current_time('timestamp') < $expiration_time ) {
-		//Añade más de 1 producto a la vez al carrito
-		foreach ($free_product_id as $product_id) {
-			$cart->add_to_cart($product_id);
-		}
+    // Check if both individual products are in cart
+    if (count(array_intersect($target_product_id_individuales, array_column($cart->get_cart(), 'product_id'))) == count($target_product_id_individuales)) {
+        // Add free products to cart
+        foreach ($free_product_id as $product_id) {
+            if (!in_array($product_id, array_column($cart->get_cart(), 'product_id'))) {
+                $cart->add_to_cart($product_id);
+            }
+        }
+    } else {
+        // Remove free products from cart if conditions are not met
+        foreach ($free_product_id as $product_id) {
+            if (in_array($product_id, array_column($cart->get_cart(), 'product_id'))) {
+                foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+                    if ($cart_item['product_id'] == $product_id) {
+                        $cart->remove_cart_item($cart_item_key);
+                    }
+                }
+            }
+        }
     }
 
-    // If the free product is already in cart and the customer was added an excluded product, remove it
-    elseif ( isset($free_key) && $product->is_on_sale() == true ) {
-        $cart->remove_cart_item( $free_key );
+    // Remove product with ID 4363 from cart if conditions are met
+    if (in_array(4363, array_column($cart->get_cart(), 'product_id')) && count(array_intersect($target_product_id_individuales, array_column($cart->get_cart(), 'product_id'))) != 1) {
+        foreach ($cart->get_cart() as $cart_item_key => $cart_item) {
+            if ($cart_item['product_id'] == 4363) {
+                $cart->remove_cart_item($cart_item_key);
+            }
+        }
     }
-	
-	$cart_items = $cart->get_cart_contents();
-	$product_ids = array();
-	foreach ($cart_items as $item) {
-		$product_ids[] = $item['product_id'];
-	}
-
-	//Este código verifica si el producto 5853 está presente en el carrito. Si no está presente o si se cumple alguna de las otras condiciones que mencionaste, entonces se elimina el producto 5870 del carrito.
-	$found_5853 = false;
-	foreach ($cart_items as $cart_item_key => $item) {
-		if ($item['product_id'] == 5853) {
-			$found_5853 = true;
-			break;
-		}
-	}
-	if (!$found_5853 || (isset($free_key) && $is_target_product_id == false) || (count($product_ids) == 2 && in_array(2817, $product_ids) && in_array(5870, $product_ids))) {
-		foreach ($cart_items as $cart_item_key => $item) {
-			if ($item['product_id'] == 5870) {
-				$cart->remove_cart_item($cart_item_key);
-				break;
-			}
-		}
-	}
-
-    // If the free product is already in cart and the customer was added an excluded product, remove it
-    elseif ( isset($free_key) && ( current_time('timestamp') < $start_time || current_time('timestamp') > $expiration_time ) ) {
-        $cart->remove_cart_item( $free_key );
-    }
-
-	//Para más de 1
-	foreach ($free_product_id as $product_id) {
-		if (isset($cart_item['product_id']) && $cart_item['product_id'] == $product_id) {
-			if ($cart_item['quantity'] > 1) {
-				$cart->set_quantity($cart_item_key, 1);
-			}
-		}
-	}
 }
